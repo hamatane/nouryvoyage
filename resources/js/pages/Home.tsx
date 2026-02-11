7d498ebf61f90824bda1417bf3db641dc0eef78c
hamatane.dev <hamatane.dev@protonmail.com>
2026-02-11 17:33:33 +0000

    update text Home and Hero

--- resources/js/pages/Home.tsx
+++ resources/js/pages/Home.tsx
@@ -3,15 +3,15 @@
 import { Head } from '@inertiajs/react';
 // import { cn } from '@/lib/utils';
 // import Blog from '../sections/Blog';
-import Contact from '../sections/Contact';
-import FAQ from '../sections/FAQ';
-import Footer from '../sections/Footer';
-import Gallery from '../sections/Gallery';
-import Hero from '../sections/Hero';
-import Navigation from '../sections/Navigation';
-import Packages from '../sections/Packages';
-import Testimonials from '../sections/Testimonials';
-import WhyChooseUs from '../sections/WhyChooseUs';
+import Contact from '@/sections/Contact';
+import FAQ from '@/sections/FAQ';
+import Footer from '@/sections/Footer';
+import Gallery from '@/sections/Gallery';
+import Hero from '@/sections/Hero';
+import Navigation from '@/sections/Navigation';
+import Packages from '@/sections/Packages';
+import Testimonials from '@/sections/Testimonials';
+import WhyChooseUs from '@/sections/WhyChooseUs';
 
 export default function Home() {
     return (
